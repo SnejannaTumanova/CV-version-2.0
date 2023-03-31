56ea47fe964814e2678daa4ce2dd6116bc668331
Snejanna_Tumanova <116222546+SnejannaTumanova@users.noreply.github.com>
2023-03-31 21:05:28 +0300

    Update code.js

--- code.js
+++ code.js
@@ -8,6 +8,8 @@ function init () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         let style = document.getElementById("style")
         style.href = "stylePhone.css"
+        let ad = document.getElementById("ad")
+        ad.innerHTML = "PS: Советую нажать на картинку снежинки &#10052; в начале страницы"
       }
 }
 
